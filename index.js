f768b7266f22d51831c7d5fd5347a68d5a8b804d
David Bautista <dj.bautista10@uniandes.edu.co>
2019-05-04 13:43:12 -0500

    v2

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const AssistantV2 = require('ibm-watson/assistant/v2');
 const app = express();
 
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 const port = 3000;
 
